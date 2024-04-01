384ddc5726987121f0a9d5b1cbd95961d5453eb9
maytronics-op <120120060+maytronics-op@users.noreply.github.com>
2024-04-01 10:19:57 +0300

    minor indenting

--- server/src/database.ts
+++ server/src/database.ts
@@ -45,10 +45,11 @@ async function applySchemaValidation(db: mongodb.Db) {
     };
 
     // Try applying the modification to the collection, if the collection doesn't exist, create it 
-   await db.command({
+    await db.command({
         collMod: "employees",
         validator: jsonSchema
-    }).catch(async (error: mongodb.MongoServerError) => {
+    })
+    .catch(async (error: mongodb.MongoServerError) => {
         if (error.codeName === "NamespaceNotFound") {
             await db.createCollection("employees", {validator: jsonSchema});
         }
